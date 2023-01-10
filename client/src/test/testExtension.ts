940d11e22e95dce93eea955c6f8d8abfbfbd56d2
EarthmanT <trammell@cloudify.co>
2023-01-10 16:30:14 -0500

    expect to be

--- client/src/test/testExtension.ts
+++ client/src/test/testExtension.ts
@@ -3,11 +3,9 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-import { ExtensionContext } from 'vscode';
-import { activate, deactivate } from './../extension';
+import { deactivate } from './../extension';
+import {expect, test} from '@jest/globals';
 
 test('test calling activate', () => {
-    const context = {} as ExtensionContext;
-    activate(context);
-    deactivate();
+    expect(deactivate()).toBe(undefined);
 });
